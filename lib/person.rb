060ecc189e4b51d23f6a2582ff88cfcac46c6f16
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-10 21:01:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complextion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
-    attributes.each do|key, value| self.send("#{key}="), value)
-    end
+    attributes.each {|key, value| self.send("#{key}="), value)}
   end
 end
   
